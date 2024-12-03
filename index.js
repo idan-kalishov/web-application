7bac9e421022895b8525aac5a61850c7e93979d5
idan-kalishov <idanKalishov@gmail.com>
2024-12-03 19:41:09 +0200

    feat: adding mongoose connection

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const app = express();
+app.use(express.json());
+
+
+mongoose
+  .connect("mongodb://localhost:27017/web-application", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.listen(3000, () => console.log(`Server running on http://localhost:3000`));
