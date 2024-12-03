43ded2b4868424f874a9e22388d5dedc0de06adc
idan-kalishov <idanKalishov@gmail.com>
2024-12-03 20:12:37 +0200

    feat: adding postController and post routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const postRoutes = require("./routes/postRoutes");
+
 
 const app = express();
 app.use(express.json());
@@ -11,5 +13,8 @@ mongoose
   })
   .then(() => console.log("connected to db"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+  app.use("/post", postRoutes);
+
 
 app.listen(3000, () => console.log(`Server running on http://localhost:3000`));
